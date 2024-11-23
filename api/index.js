2cc3b2b9066246a8f607d579996ac0b3498fa447
JayeshKarankal3425 <jayeshkarankal0@gmail.com>
2024-11-23 21:23:53 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import router from "../routers/feedbackRouter.js";
+import dbConnect from "../config/database.js";
+
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;  
+
+
+app.use(express.json());
+
+
+app.use("/api/v1", router);
+
+dbConnect();
+app.get("/",(req, res) => {
+  res.json({
+    "success": true,
+    "message": "server running successfully!",
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`);
+});
